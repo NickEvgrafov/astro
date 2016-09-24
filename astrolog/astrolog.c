9c16fbe59138dcd9caf1240500d4a51cb03ded70
NickEvgrafov <NickEvgrafov@gmail.com>
2016-09-24 15:49:44 +0300

    Сборочный файл для сервера-астролога

---
+++ astrolog/astrolog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(int argc, char** argv)
+{
+    printf("Starting astrolog\n");
+    printf("Finishing astrolog\n");
+    exit(EXIT_SUCCESS);
+}
