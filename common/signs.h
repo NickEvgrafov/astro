fb94c8e961fe151333cc19e637944e983a7d38c4
NickEvgrafov <NickEvgrafov@gmail.com>
2016-10-01 16:20:45 +0300

    All zodiac signs

---
+++ common/signs.h
@@ -0,0 +1,26 @@
+/**
+ * Константы знаков зодиака
+ */
+
+#ifndef __SIGNS_H__
+#define __SIGNS_H__
+
+const char[12] aries        = "Aries       ";
+const char[12] taurus       = "Taurus      ";
+const char[12] gemini       = "Gemini      ";
+const char[12] cancer       = "Cancer      ";
+const char[12] leo          = "Leo         ";
+const char[12] virgo        = "Virgo       ";
+const char[12] libra        = "Libra       ";
+const char[12] scorpio      = "Scorpio     ";
+const char[12] saggitarious = "Saggitarious";
+const char[12] capricorn    = "Capricorn   ";
+const char[12] aquarious    = "Aquarious   ";
+const char[12] pisces       = "Pisces      ";
+
+const char[12][12] = {
+    aries, taurus, gemini, cancer, leo, virgo,
+    libra, scorpio, saggitarious, capricorn, aquarious, pisces
+};
+
+#endif
