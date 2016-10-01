de0eedde9ca6f0006a688adeb6ccd4dd76a9c9ae
NickEvgrafov <NickEvgrafov@gmail.com>
2016-10-01 16:21:11 +0300

    all horoscopes

---
+++ star/horoscopes.h
@@ -0,0 +1,35 @@
+/**
+ * horoscopes.h - всевозможные прогнозы
+ */
+#ifndef __HOROSCOPES_H__
+#define __HOROSCOPES_H__
+
+const char[24][80] horoscopes =
+{
+    "Your romantic skills might not be as sharp as you...                           \n", // 1
+    "If other people around you get loud and angry...                               \n", // 2
+    "Every good thing must come to an end, and you'll...                            \n", // 3
+    "You're coming to the close of a serious...                                     \n", // 4
+    "There are a lot of different emotions churning...                              \n", // 5
+    "It's awfully tempting to let other people take...                              \n", // 6
+    "You might not feel comfortable being a joiner,...                              \n", // 7
+    "Ok, so you might not exactly be in love with your...                           \n", // 8
+    "This is a great day for action, but it's not such...                           \n", // 9
+    "It's not going to be an easy day for you, but it...                            \n", // 10
+    "Exploration will be the best thing for getting...                              \n", // 11
+    "You've got more freedom than ever before -- are...                             \n", // 12
+    "Your imagination is working in your favor...                                   \n", // 13
+    "You are getting into a place of understanding about recent events...           \n", // 14
+    "This is a fine day for compassion and charm as well...                         \n", // 15
+    "You are expressing yourself in ways that are especially relatable to others... \n", // 16
+    "This is a good day for some healthy escapism...                                \n", // 17
+    "A partner or friend's gentle touch or conversation can help you relax...       \n", // 18
+    "There is good energy for expressing your care and concern verbally...          \n", // 19
+    "People are understanding the intention behind the message...                   \n", // 20
+    "Pay close attention to dreams and daydreams today...                           \n", // 21
+    "Financial instincts can be healthy now, but your approach is imaginative...    \n", // 22
+    "Watch for speaking from your mind and not your heart tonight...                \n", // 23
+    "Aim to learn from differences...                                               \n", // 24
+};
+
+#endif
